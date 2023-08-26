5d39bd3e9e091d9a4f79092d8b73da5bb659b845
Aidan Loughran <aidan@loughran.dev>
2023-08-26 21:04:54 +0100

    fix(svelte): specify `app()` return type

--- packages/lib-svelte/src/lib/functions/app.ts
+++ packages/lib-svelte/src/lib/functions/app.ts
@@ -1,6 +1,7 @@
 import { getContext } from 'svelte';
+import type { TrusyncApp } from 'trusync';
 import { APP_KEY } from '$lib/constants/context-keys';
 
-export function app() {
+export function app(): TrusyncApp {
   return getContext(APP_KEY);
 }

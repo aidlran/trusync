51000deb0674fe25f251e6313ef64ce17598485d
Aidan Loughran <aidan@loughran.dev>
2023-08-26 18:01:41 +0100

    svelte: delete `use:autofocus`

--- packages/lib-svelte/src/lib/functions/use/autofocus.ts
+++
@@ -1,3 +0,0 @@
-export function autofocus(node: HTMLElement) {
-  node.focus();
-}
--- packages/lib-svelte/src/lib/index.ts
+++ packages/lib-svelte/src/lib/index.ts
@@ -3,7 +3,6 @@ import SignedIn from './components/signed-in.svelte';
 export * from './functions/data.js';
 export * from './functions/init.js';
 export * from './functions/root.js';
-export * from './functions/use/autofocus.js';
 export * from './interfaces/data-store.js';
 
 export { SignedIn };

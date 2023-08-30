7a109ddd3fd8c5d271933c22feb211101e2198fa
Aidan Loughran <aidan@loughran.dev>
2023-08-30 20:33:28 +0100

    feat: `getNamedJSON`

--- packages/lib-js/src/app/trusync-app.ts
+++ packages/lib-js/src/app/trusync-app.ts
@@ -3,7 +3,7 @@ import { Hash } from '../storage/interfaces/hash';
 import { StorageDriver } from '../storage/interfaces/storage-driver';
 
 /**
- * Constructs a Trusync app, which is configured using the builder pattern.
+ * A truSync app instance.
  */
 export class TrusyncApp {
   private readonly internalStorageDrivers = new Array<StorageDriver>();
@@ -49,7 +49,7 @@ export class TrusyncApp {
     };
   }
 
-  private async put(payload: string, mediaType = 'text/plain'): Promise<boolean> {
+  async put(payload: string, mediaType = 'text/plain'): Promise<boolean> {
     const hash = await this.hash(payload);
     const results = await Promise.allSettled(
       this.internalStorageDrivers.map(async (store) => {
@@ -64,15 +64,11 @@ export class TrusyncApp {
     return !!results.find((result) => result.status === 'fulfilled');
   }
 
-  private async putJSON(payload: object): Promise<boolean> {
+  async putJSON<T>(payload: T): Promise<boolean> {
     return this.put(JSON.stringify(payload), 'application/json');
   }
 
-  private async putNamed(
-    payload: string,
-    name: string,
-    mediaType = 'text/plain',
-  ): Promise<boolean> {
+  async putNamed(payload: string, name: string, mediaType = 'text/plain'): Promise<boolean> {
     const hash = await this.hash(payload);
     const results = await Promise.allSettled(
       this.internalStorageDrivers.map(async (store) => {
@@ -88,8 +84,37 @@ export class TrusyncApp {
     return !!results.find((result) => result.status === 'fulfilled');
   }
 
-  private async putNamedJSON(payload: object, name: string): Promise<boolean> {
+  async putNamedJSON<T>(payload: T, name: string): Promise<boolean> {
     return this.putNamed(JSON.stringify(payload), name, 'application/json');
+  }
+
+  getNamedJSON<T>(name: string): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      void Promise.allSettled<Promise<boolean>[]>(
+        this.internalStorageDrivers.map(async (store) => {
+          const hash = await store.getNamedDataHash(name);
+          if (!hash) return false;
+          const data = await store.getData(hash);
+          if (!data || data.hash.algorithm !== hash.algorithm || data.hash.value !== hash.value) {
+            return false;
+          }
+          const validateHash = await this.hash(data.payload);
+          if (
+            data.hash.algorithm !== validateHash.algorithm ||
+            data.hash.value !== validateHash.value
+          ) {
+            return false;
+          }
+          resolve(JSON.parse(data.payload) as T);
+          return true;
+        }),
+      ).then((results) => {
+        console.log(results);
+        if (!results.find((promise) => promise.status === 'fulfilled' && promise.value)) {
+          reject(new Error(`truSync: named data not found: '${name}'`));
+        }
+      });
+    });
   }
 
   async createIdentity(): Promise<{

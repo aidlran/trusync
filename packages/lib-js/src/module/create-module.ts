2adfd8cd7c7502255bcf31ae1a50d2eff4566e4a
Aidan Loughran <aidan@loughran.dev>
2023-11-27 18:36:24 +0000

    refactor: createModule

--- packages/lib-js/src/module/create-module.ts
+++ packages/lib-js/src/module/create-module.ts
@@ -1,14 +1,16 @@
-const DEPENDENCY_SET = new Set<unknown>();
+const DEPENDENCIES = new Set();
 
 export function createModule<T>(constructor: (appID: string) => T): (appID?: string) => T {
   const INSTANCES: Record<string, T> = {};
   return (appID = ''): T => {
-    if (DEPENDENCY_SET.has(constructor)) {
+    if (DEPENDENCIES.has(constructor)) {
       throw new Error('Circular dependency detected');
     }
-    DEPENDENCY_SET.add(constructor);
-    const module = (INSTANCES[appID] ??= constructor(appID));
-    DEPENDENCY_SET.delete(constructor);
-    return module;
+    DEPENDENCIES.add(constructor);
+    if (!INSTANCES[appID]) {
+      INSTANCES[appID] = constructor(appID);
+    }
+    DEPENDENCIES.delete(constructor);
+    return INSTANCES[appID];
   };
 }

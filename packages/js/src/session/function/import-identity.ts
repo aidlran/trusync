a8c18605c3f521c91ac91a04d8568583d8ae132c
Aidan Loughran <aidan@loughran.dev>
2023-12-06 23:57:59 +0000

    fix: import identity to anon session

--- packages/js/src/session/function/import-identity.ts
+++ packages/js/src/session/function/import-identity.ts
@@ -43,7 +43,7 @@ export function importIdentity(
         } else {
           return {
             active: true,
-            identities: new Set(address),
+            identities: new Set([address]),
           };
         }
       });

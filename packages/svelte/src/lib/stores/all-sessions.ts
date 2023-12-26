0b432dacba1ec0dc1487c7e96437d6075419e7f4
Aidan Loughran <aidan@loughran.dev>
2023-12-26 09:28:12 +0000

    fix: all sessions export

--- packages/svelte/src/lib/stores/all-sessions.ts
+++ packages/svelte/src/lib/stores/all-sessions.ts
@@ -1,17 +1,17 @@
 import { type Readable, readable } from 'svelte/store';
-import { type Sessions, session } from 'trusync';
+import { type AllSessions, session } from 'trusync';
 
-export function allSessions<T = unknown>(appID?: string): Readable<Sessions<T>> {
+export function allSessions<T = unknown>(appID?: string): Readable<AllSessions<T>> {
   if (!globalThis.window) {
     return readable();
   }
 
   // https://svelte.dev/docs/svelte-store
-  return readable<Sessions<T>>(undefined, (update) => {
+  return readable<AllSessions<T>>(undefined, (update) => {
     // "[...] called when the number of subscribers goes from zero to one [...]"
     // "[...] return a stop function that is called when the subscriber count goes from one to zero."
     return session(appID).onSessionsChange((sessions) => {
-      update(sessions as Sessions<T>);
+      update(sessions as AllSessions<T>);
     });
   });
 }
